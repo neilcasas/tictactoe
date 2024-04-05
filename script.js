0e7d0c67c9ff436488c6292010d486bde5186896
Neil Casas <neilalfonzcasas@gmail.com>
2024-04-05 22:13:46 +0800

    Added initial game container grid

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const gameContainer = (function () {
-    const gameContainerListener = document.getElementById('game-container');
-    gameContainerListener.addEventListener('click', () => {
-        alert("This works");
-    })
+    const gameContainerElement = document.getElementById('game-container');
+
+    // create single cell
+    function createCell(index) {
+        let cell = document.createElement('div');
+        cell.setAttribute('class', 'cell');
+        cell.setAttribute('index', index);
+        cell.addEventListener('click', () => {
+            cell.textContent = "X";
+        })
+        return cell;
+    }
+
+    // populate game container grid by 9 cells
+    function populateGrid() {
+        for (let i = 0; i < 9; i++) {
+            gameContainerElement.appendChild(createCell(i));
+        }
+    };
+    populateGrid();
 })();
 
