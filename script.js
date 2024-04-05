700428011930d60842d5b9a8c039fd9b0c2881b4
Neil Casas <neilalfonzcasas@gmail.com>
2024-04-05 22:40:16 +0800

    Created factory function for button object and element

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const gameContainer = (function () {
     const gameContainerElement = document.getElementById('game-container');
+    let cells = []; // array containing cells;
+    let turn = true; // true denotes X's turn, false denotes O's turn
 
-    // create single cell
-    function createCell(index) {
+    // create cell object
+    function createCellObject(index, turn) {
+        return {
+            index,
+            text: '',
+            turn,
+            displayText() {
+                this.text = this.turn ? 'X' : 'O';
+            }
+        }
+    }
+
+    // create cell DOM element
+    function createCellElement(cellObject) {
         let cell = document.createElement('div');
         cell.setAttribute('class', 'cell');
-        cell.setAttribute('index', index);
+        cell.setAttribute('index', cellObject.index);
         cell.addEventListener('click', () => {
-            cell.textContent = "X";
+            cellObject.displayText();
+            cell.innerHTML = cellObject.text;
         })
         return cell;
     }
@@ -15,9 +30,14 @@ const gameContainer = (function () {
     // populate game container grid by 9 cells
     function populateGrid() {
         for (let i = 0; i < 9; i++) {
-            gameContainerElement.appendChild(createCell(i));
+            let cellObject = createCellObject(i, turn);
+            cells.push(cellObject); // push each cell object to array
+            gameContainerElement.appendChild(createCellElement(cellObject));
         }
     };
     populateGrid();
+
+    // reset game
+    const resetButton = document.getElementById()
 })();
 
